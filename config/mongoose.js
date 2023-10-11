2913d3195ea1b24da5de6b8c572de95c65176a95
unknown <ankurt0419@gmail.com>
2023-10-11 11:00:45 +0530

    connected to db and created layouts

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost:27017/codialDB');
+
+const db=mongoose.connection;
+
+db.on('error',(err)=>{
+    console.log(err);
+})
+
+db.once('open',()=>{
+    console.log("successfuly connected to db");
+})
+
+module.exports=db;
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express=require('express');
 const path=require('path');
+const layouts=require('express-ejs-layouts');
 const port=8000;
+
+const db=require('./config/mongoose');
 
 const app=express();
 
+app.use(express.static('./assets'));
+//to move style and script link from sub pages into the layout head
+app.set('layout extractStyles', true);
+
+app.use(layouts);
 //with the help of below middleware we can pass our route handling part to router 
 app.use('/',require('./routes'));   
 app.set('view engine','ejs');
