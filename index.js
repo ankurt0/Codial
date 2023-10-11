70738752ed9698c8676250a9b3dacf743309f190
unknown <ankurt0419@gmail.com>
2023-10-11 08:42:34 +0530

    Added basic routes, controller and view

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express=require('express');
+const path=require('path');
 const port=8000;
 
 const app=express();
+
+//with the help of below middleware we can pass our route handling part to router 
+app.use('/',require('./routes'));   
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 app.listen(port,function(err){
     if(err)
